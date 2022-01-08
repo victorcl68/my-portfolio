4b7d7a469ca2ffcc6b85899166f00722fe9caa57
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2022-01-08 16:18:10 -0300

    Update README, and adding some projects

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
- # Portfólio de Victor Lopes
+ # [Portfólio de Victor Lopes](https://victorcl68.github.io/my-portfolio/)
  
  ## Repositórios de cada projeto
-  * To-do List: https://github.com/victorcl68/to-do-list
-  * Movie Card Library CRUD: https://github.com/victorcl68/movie-card-library-CRUD
-  * Pixels Art: https://github.com/victorcl68/pixels-art
-  * Carrinho de Compras: https://github.com/victorcl68/shopping-cart
-  * Lições Aprendidas: https://github.com/victorcl68/lessons-learned
+  * [Web Chat](https://github.com/victorcl68/webchat)
+  * [Carrinho de Compras](https://github.com/victorcl68/shopping-cart)
+  * [To-do List](https://github.com/victorcl68/to-do-list)
+  * [Pixels Art](https://github.com/victorcl68/pixels-art)
+  * [Aplicativo de Receitas](https://github.com/victorcl68/recipes-app)
+  * [Movie Card Library CRUD](https://github.com/victorcl68/movie-card-library-CRUD)
 
-##
+---
 
 ### Observação
- * O projeto **Movie Card Library CRUD** está hospedado na plataforma do Heroku, e a mesma após um tempo de inatividade suspende a aplicação, peço que espere alguns segundos após entrar no site para que ela volte ao ar.
+O projeto **Movie Card Library CRUD**, **Web Chat** e **Aplicativo de Receitas** estão hospedados na plataforma Heroku, e após um tempo de inatividade ela suspende essas aplicações, peço que espere alguns segundos após entrar no site para que ela volte ao ar.
--- src/App.js
+++ src/App.js
@@ -10,38 +10,40 @@ function App() {
         <h2 id="subtitle">
           Desde pequeno apaixonado por tecnologia estudo desenvolvimento web na Trybe.
           <br />
-          Tenho conhecimento em React.js e um nível de inglês intermediário.
+          Tenho conhecimento em NodeJS e um nível de inglês intermediário.
           <br />
           Acredito e trabalho para poder fazer diferença positiva na vida das pessoas.
         </h2>
         <section id="uls">
           <ul id="abilities">
             <h2 className="title-ul"> Principais Habilidades </h2>
-            <li className="item"> React.js | Redux | Hooks </li>
-            <li className="item"> React Testing Library </li>
-            <li className="item"> DOM | JavaScript ES6 </li>
-            <li className="item"> Testes Unitários </li>
+            <li className="item"> NodeJS | JavaScript </li>
+            <li className="item"> MySQL | MongoDB </li>
             <li className="item"> Bash | Unix </li>
             <li className="item"> Git | Github </li>
             <li className="item"> CSS | HTML </li>
+            <li className="item"> Testes Unitários </li>
           </ul>
           <ul id="projects">
             <h2 className="title-ul"> Projetos </h2>
-            <li><a href="https://victorcl68.github.io/to-do-list/" target="_blank" rel="noreferrer">
-              <h3 className="item"> To-Do List </h3>
-            </a></li>
-            <li><a href="https://movie-card-library.herokuapp.com/ " target="_blank" rel="noreferrer">
-              <h3 className="item"> Movie Cards Library </h3>
-            </a></li>
-            <li><a href="https://victorcl68.github.io/pixels-art/" target="_blank" rel="noreferrer">
-              <h3 className="item"> Pixels Art </h3>
-            </a></li>
-            <li><a href="https://victorcl68.github.io/shopping-cart/" target="_blank" rel="noreferrer">
-              <h3 className="item"> Carrinho de Compras </h3>
-            </a></li>
-            <li><a href="https://victorcl68.github.io/lessons-learned/" target="_blank" rel="noreferrer">
-              <h3 className="item"> Lessons learned </h3>
-            </a></li>
+              <li><a href="https://webchat-victor.herokuapp.com" target="_blank" rel="noreferrer">
+                <h3 className="item"> Web Chat </h3>
+              </a></li>
+              <li><a href="https://victorcl68.github.io/shopping-cart/" target="_blank" rel="noreferrer">
+                <h3 className="item"> Carrinho de Compras </h3>
+              </a></li>
+              <li><a href="https://victorcl68.github.io/to-do-list/" target="_blank" rel="noreferrer">
+                <h3 className="item"> To-Do List </h3>
+              </a></li>
+              <li><a href="https://victorcl68.github.io/pixels-art/" target="_blank" rel="noreferrer">
+                <h3 className="item"> Pixels Art </h3>
+              </a></li>
+              <li><a href="https://project-recipes-app.herokuapp.com/" target="_blank" rel="noreferrer">
+                <h3 className="item"> Recipe App </h3>
+              </a></li>
+              <li><a href="https://movie-card-library.herokuapp.com/" target="_blank" rel="noreferrer">
+                <h3 className="item"> Movie Cards Library </h3>
+              </a></li>
           </ul>
         </section>
         <p id="contact">Contato</p>
